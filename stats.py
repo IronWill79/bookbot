46cb34754e7a1d30ee365b1b5c9cb254b5ecfb24
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 14:24:50 +1000

    update stats.py
    - add `CharCount` TypedDict
    - add `sort_on_counts` function for use in `.sort()`
    - add `get_sorted_char_counts` function for character report

--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from typing import TypedDict
+
 def count_words(text: str) -> int:
     return len(text.split())
 
@@ -11,3 +13,17 @@ def count_chars(text: str) -> dict[str, int]:
         else:
             char_counts[char] = 1
     return char_counts
+
+
+CharCount = TypedDict('CharCount', {'char': str, 'num': int})
+
+def sort_on_counts(char_counts: CharCount) -> int:
+    return char_counts["num"]
+
+
+def get_sorted_char_counts(char_counts: dict[str, int]) -> list[CharCount]:
+    char_count_list: list[CharCount] = []
+    for char in char_counts:
+        char_count_list.append({"char": char, "num": char_counts[char]})
+    char_count_list.sort(reverse=True, key=sort_on_counts)
+    return char_count_list
