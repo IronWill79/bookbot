2d80085401b4bc6d8846b8ea0fe8fd96e7f30030
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 14:03:46 +1000

    update stats.py
    - Add type hints

--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
-def count_words(text):
+def count_words(text: str) -> int:
     return len(text.split())
 
 
-def count_chars(text):
-    char_counts = {}
+def count_chars(text: str) -> dict[str, int]:
+    char_counts: dict[str, int] = {}
     lower_text = text.lower()
     for char in lower_text:
         if char in char_counts:
