c222e0db40fcc4cbde09aab5613d0d47e1d787fc
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 13:48:32 +1000

    update main.py
    - output result of `count_chars` function after word count

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import count_words
+from stats import count_chars, count_words
 
 def main():
     text = get_book_text('./books/frankenstein.txt')
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
+    char_counts = count_chars(text)
+    print(char_counts)
 
 
 def get_book_text(filepath):
