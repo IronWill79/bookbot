83f0670efcaed2b7e292954302cffc9ced430920
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 13:44:23 +1000

    update main.py
    - remove and import `count_words` function

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import count_words
+
 def main():
     text = get_book_text('./books/frankenstein.txt')
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
-
-
-def count_words(text):
-    return len(text.split())
 
 
 def get_book_text(filepath):
