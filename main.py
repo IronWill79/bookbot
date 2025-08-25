e5186de9d2c51f326cd181268243a0d0d4a6efc3
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 13:14:32 +1000

    update main.py
    - Read and display `books/frankenstein.txt`

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def main():
+    text = get_book_text('./books/frankenstein.txt')
+    print(text)
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+main()
