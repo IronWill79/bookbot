503ef026c82e92a2f3ca0903d49dcc2b759ca816
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 14:25:37 +1000

    update main.py
    - add `print_report_of_file` function to print report on console
    - update `main` function to call new function

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from stats import count_chars, count_words
+from stats import count_chars, count_words, get_sorted_char_counts
 
 def main() -> None:
-    text = get_book_text('./books/frankenstein.txt')
-    num_words = count_words(text)
-    print(f"{num_words} words found in the document")
-    char_counts = count_chars(text)
-    print(char_counts)
+    print_report_of_file("books/frankenstein.txt")
 
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+
+def print_report_of_file(filepath: str) -> None:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    text = get_book_text(filepath)
+    word_count = count_words(text)
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    char_counts = get_sorted_char_counts(count_chars(text))
+    for char in char_counts:
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
 
 
 main()
