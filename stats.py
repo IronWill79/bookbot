50bceb62e3635ceea0e6898ef31df74fea96f613
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 13:47:51 +1000

    update stats.py
    - add `count_chars` function

--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def count_words(text):
     return len(text.split())
+
+
+def count_chars(text):
+    char_counts = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
